e70a07c29bf9d427de3858bfb8a9175feb899677
Matias-15 <mcriado.cg@gmail.com>
2021-09-01 19:56:13 -0400

    Initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyACBg2JfTYP5q5dVn7oFQqlWoVB0TSlL1o',
+  authDomain: 'authenticator-boolean-app.firebaseapp.com',
+  projectId: 'authenticator-boolean-app',
+  storageBucket: 'authenticator-boolean-app.appspot.com',
+  messagingSenderId: '617584222394',
+  appId: '1:617584222394:web:2bc4297a3a4d87910fe283'
+}
