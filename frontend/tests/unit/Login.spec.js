be44848e295713365a4da4f9d969601c6cb80a89
Matias-15 <mcriado.cg@gmail.com>
2021-09-08 20:44:31 -0300

    Fix bugs test unit alert in frontend

--- frontend/tests/unit/Login.spec.js
+++ frontend/tests/unit/Login.spec.js
@@ -61,7 +61,7 @@ describe('Login.vue', () => {
     expect(firebaseApp.auth().signInWithEmailAndPassword).not.toHaveBeenCalled()
   })
 
-  it.skip('Shows the global alert when authentication fails ', async () => {
+  it('Shows the global alert when authentication fails ', async () => {
     const wrapper = mount(App, {
       localVue,
       vuetify,
